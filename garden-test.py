6a5142f28056d578e61e775315a5c4af59dbc88f
ckcole <cole.ckc@gmail.com>
2018-08-07 12:12:26 -0700

    get_largest_adjacent and run bunny logic

--- garden-test.py
+++ garden-test.py
@@ -1,26 +1,3 @@
-
-
-# Test case
-garden1 = [
-[5, 7, 8, 6, 3],
-[0, 0, 7, 0, 4],
-[4, 6, 3, 4, 9],
-[3, 1, 0, 5, 8]]
-
-garden2 = [
-[5, 7, 8, 3],
-[0, 0, 7, 4],
-[4, 6, 3, 9],
-[3, 1, 0, 8]]
-
-garden3 = [
-[5, 7, 8, 6, 3],
-[0, 0, 7, 0, 4],
-[4, 6, 3, 4, 9],
-[4, 6, 3, 4, 9],
-[3, 1, 0, 5, 8]]
-
-
 def find_center(matrix):
     '''
     Return an x,y coord thats either the dead center of the matrix,
@@ -59,8 +36,63 @@ def find_center(matrix):
     return largest_cell
 
 
-if __name__ == '__main__':
-    cells = find_center(garden1)
-    print cells
-    print garden1[cells[0]][cells[1]]
+def get_largest_adjacent(cell, matrix):
+    x, y = cell
+    adj = []
+    #  Collect all cells to the right, left, above, and below current cell.
+    if x != 0:
+        adj.append((x - 1, y))
+    if y != 0:
+        adj.append((x, y - 1))
+    if x != len(matrix) - 1:
+        adj.append((x + 1, y))
+    if y != len(matrix) - 1:
+        adj.append((x, y + 1))
 
+    # return the cell with the most carrots.  if all cells are empty,  return None and rabbit sleeps
+    largest = 0
+    largest_cell = None
+    for a_cell in adj:
+        if matrix[a_cell[0]][a_cell[1]] > largest:
+            largest = matrix[a_cell[0]][a_cell[1]]
+            largest_cell = a_cell
+    return largest_cell
+
+
+def run(matrix):
+    x, y = find_center(matrix)
+    carrots_eaten = matrix[x][y]
+    adj = get_largest_adjacent((x, y), matrix)
+    while adj is not None:
+        carrots_eaten += matrix[adj[0]][adj[1]]
+        matrix[adj[0]][adj[1]] = 0
+        adj = get_largest_adjacent(adj, matrix)
+    return carrots_eaten
+
+
+# Test cases
+garden1 = [
+    [5, 7, 8, 6, 3],
+    [0, 0, 7, 0, 4],
+    [4, 6, 3, 4, 9],
+    [3, 1, 0, 5, 8]]
+
+garden2 = [
+    [5, 7, 8, 3],
+    [0, 0, 7, 4],
+    [4, 6, 3, 9],
+    [3, 1, 0, 8]]
+
+garden3 = [
+    [5, 7, 8, 6, 3],
+    [0, 0, 7, 0, 4],
+    [4, 6, 3, 4, 9],
+    [4, 6, 3, 4, 9],
+    [3, 1, 0, 5, 8]]
+
+
+if __name__ == '__main__':
+    test = run(garden1)
+    print 'test case returned {}'.format(test)
+    if test == 27:
+        print 'Passed'
